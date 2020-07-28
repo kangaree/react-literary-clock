cf3637429336e7c788b0cbdbff8cfd87034a76ad
Gary Kang <garyzkang@gmail.com>
2020-07-28 12:25:30 -0400

    Boys, do it now. God's time is 12.25 . Ulysses

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const now = new Date();
+  const [hour, setHour] = useState(now.getHours());
+  const [minute, setMinute] = useState(now.getMinutes());
+  const [nowText, setNowText] = useState('');
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date();
+      setHour(now.getHours());
+      setMinute(now.getMinutes());
+
+      const nowTextArray = require(`./times/${("0" + hour).slice(-2)}_${(
+        "0" + minute
+      ).slice(-2)}.json`);
+      // const randomNowText =
+      //   nowTextArray[Math.floor(Math.random() * nowTextArray.length)];
+      const firstText = nowTextArray[0];
+
+      setNowText(firstText);
+    }, 1 * 1000);
+    return () => clearInterval(timer);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{ fontSize: 32 }}>
+          {nowText.quote_first}
+          <code style={{ color: "#61dafb" }}>
+            {nowText.quote_time_case}
+          </code>{" "}
+          {nowText.quote_last}
+        </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <i>{nowText.title}</i> by {nowText.author}{" "}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instructables.com/id/Literary-Clock-Made-From-E-reader/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Inspired by Literary Clock E-reader
         </a>
       </header>
     </div>
