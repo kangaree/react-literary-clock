3e7593cd85f2e05d293e66422c5ac6543d7cf1d6
Gary Kang <garyzkang@gmail.com>
2020-07-28 13:03:26 -0400

    Updates

---
+++ precache-manifest.8bcb8185b1457715cd9728d8bd4e1de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5d5d8b3cbcc645d5a89b2ee64daad8",
+    "url": "/react-literature-clock/index.html"
+  },
+  {
+    "revision": "b16bd2ddeb77554fd130",
+    "url": "/react-literature-clock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6edc33cbdbe4d6e3f19",
+    "url": "/react-literature-clock/static/js/2.6c322779.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-literature-clock/static/js/2.6c322779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16bd2ddeb77554fd130",
+    "url": "/react-literature-clock/static/js/main.7eeac19f.chunk.js"
+  },
+  {
+    "revision": "e0b3c68904181aaac20b",
+    "url": "/react-literature-clock/static/js/runtime-main.2c5caeaf.js"
+  }
+]);
